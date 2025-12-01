b79c443b41c799a55ba973463ef7fbe98de8ddaa
akashpandey1959k-dotcom <akashpandey1959k@gmail.com>
2025-12-02 00:02:56 +0530

    Add files via upload

---
+++ akash.js
@@ -0,0 +1,11 @@
+alert("this is an external java script file.");
+
+function myfun()
+
+{window.alert ("data is submitted successfully");
+
+}
+
+var fullname="John";
+let age= 25;
+const country="india"
